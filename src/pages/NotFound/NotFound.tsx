cc702734ef9c97a5b2723762c0c25939e6ae0b2a
Bruno Da Silva <hi.dasilvab@gmail.com>
2025-06-27 10:14:43 -0500

    configured, 404 and styled it

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,18 @@
+import "./notFound.css";
 export const NotFound = () => {
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className="not-found-container">
+      <div className="error-code">
+        <span className="digit">4</span>
+        <span className="digit">0</span>
+        <span className="digit">4</span>
+      </div>
+      <div className="message">
+        Oops! The page you're looking for doesn't exist.
+      </div>
+      <a href="/" className="home-link">
+        Go back home
+      </a>
     </div>
   );
 };
